3cd70b32af874e707a255cf93668d264a386cca2
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-09-12 20:27:35 +0200

    setting cname endpoint

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const dns = require("dns");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json("Hello, Mano");
+app.get("/cname", (req, res) => {
+  const domain = req.query.domain;
+
+  if (!domain) {
+    return res.status(400).json({ error: "Domain parameter is required." });
+  }
+
+  dns.resolveCname(domain, (err, addresses) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ cname: addresses });
+  });
 });
 
 app.listen(PORT, () => {
